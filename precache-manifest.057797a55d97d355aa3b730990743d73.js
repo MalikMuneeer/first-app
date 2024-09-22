c7db2acb32eaf5e44b9a5d76a7df3a2b9478a8e2
Malik Muneer Hussain <107171742+MalikMuneeer@users.noreply.github.com>
2024-09-22 22:26:43 +0500

    Updates

---
+++ precache-manifest.057797a55d97d355aa3b730990743d73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc4e55d9eb00b31fb2b7b5d1b8b0f79",
+    "url": "/first-app/index.html"
+  },
+  {
+    "revision": "dcb4f9b955c170303208",
+    "url": "/first-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "df18eeb4ec490e9c8dd4",
+    "url": "/first-app/static/js/2.39f777ab.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/first-app/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "dcb4f9b955c170303208",
+    "url": "/first-app/static/js/main.ce965359.chunk.js"
+  },
+  {
+    "revision": "72d71e210d78c0d44d0f",
+    "url": "/first-app/static/js/runtime~main.a219616d.js"
+  }
+]);
